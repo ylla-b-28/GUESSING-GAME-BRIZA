45d9440ca87299c0ae13c8dd7beb3f0715f2d5b3
ylla-b-28 <ylla.briza28@gmail.com>
2026-04-29 04:21:46 +0800

    I added a class and added an except if file is not found.

--- text_file_reader/odd_and_even_text_reader/odd_and_even_file_text_reader.py
+++ text_file_reader/odd_and_even_text_reader/odd_and_even_file_text_reader.py
@@ -1,17 +1,29 @@
-def separate_even_and_odd(file_to_read):
-    with open(file_to_read, 'r')  as source_file:
-        number_data = source_file.readlines()
+class OddEvenNumberSorter:
 
-#open the file
-with open ('even.txt', 'w') as even_file, open ('odd.txt', 'w') as odd_file:
-#read every line
-    for line in number_data:
-        number_value = int(line.strip())
+    def __init__(self, target_file):
+        self.target_file = target_file
+        self.even_results = "even.txt"
+        self.odd_results = "odd.txt"
 
-    if number_value % 2 == 0:
-        even_file.write(str(number_value) + '\n')
-    else:
-        odd_file.write(str(number_value) + '\n')
+    def sort_the_numbers(self):
+        try:
+            with open(self.target_file, "r") as file_to_read:
+                lines_from_file = file_to_read.readlines()
 
-separate_even_and_odd("numbers.txt")
-print("Files have been updated!")
+            with open(self.even_results, "w") as even_file, open(self.odd_results, "w") as odd_file:
+                for line in lines_from_file:
+                    single_number = int(line.strip())
+
+                    if single_number % 2 == 0:
+                        even_file.write(str(number_value) + '\n')
+                    else:
+                        odd_file.write(str(number_value) + '\n')
+
+            print("Successfully sorted the numbers into even.txt and odd.txt!")
+
+        except FileNotFoundError:
+            print("The file numbers.txt was not found. Please make it first.")
+
+if __name__ == "__main__":
+    my_sorter = OddEvenNumberSorter("numbers.txt")
+    my_sorter.sort_the_numbers()
