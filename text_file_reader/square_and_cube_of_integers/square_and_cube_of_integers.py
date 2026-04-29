e8cc1b657fa27f5c8c7cd925b5c0cd9ef8c92859
ylla-b-28 <ylla.briza28@gmail.com>
2026-04-29 16:32:58 +0800

    I added the variables and the structure.

--- text_file_reader/square_and_cube_of_integers/square_and_cube_of_integers.py
+++ text_file_reader/square_and_cube_of_integers/square_and_cube_of_integers.py
@@ -1,7 +1,14 @@
-#split numbers
+with open("integers.txt", "r") as source:
+    file_double = open("double.txt", "w")
+    file_triple = open("triple.txt", "w")
 
-#variable
+    for line in source:
+        num = int(line.strip())
+        if num % 2 == 0:
+           file_double.write(str(num ** 2) + "\n")
+        else:
+           file_triple.write(str(num ** 3) + "\n")
 
-#operation
-
-#close
+    file_double.close()
+    file_triple.close()
+print("Files generated.")
